c5d6cb255e5ed8ac4d3987a73c117871ed40e050
okemmm <131091828+okemmm@users.noreply.github.com>
2023-12-24 17:45:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // DOMが完全に読み込まれた後に実行されるコード
 document.addEventListener('DOMContentLoaded', function() {
     function loadTouristInfo() {
-        var touristInfo = '京都は、千年以上日本の首都であり続け、多くの古い寺院、文化遺産が残る街です。';
-        document.getElementById('touristInfo').innerText = touristInfo;
+        var element = document.getElementById('touristInfo');
+        if (element) {
+            element.innerText = '京都は、千年以上日本の首都であり続け、多くの古い寺院、文化遺産が残る街です。';
+        }
     }
 
     loadTouristInfo();
