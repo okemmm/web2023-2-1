41b5e9157c0d17411a211cf7eca1ddb49c1181aa
okemmm <131091828+okemmm@users.noreply.github.com>
2023-12-24 16:04:04 +0900

    Update and rename ex-7-11.js to script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// ここにJavaScriptコードを書いていく
+document.addEventListener('DOMContentLoaded', function() {
+    // DOMが完全に読み込まれた後に実行されるコード
+    var element = document.getElementById('someElementId');
+    // elementに対する操作
+});
